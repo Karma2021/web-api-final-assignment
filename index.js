8a082efc414a18ca45a108d565e9d87ec9c2b811
Karma2021 <lamak0068@gmail.com>
2023-05-26 22:27:15 +0545

    file added

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const usersRouter = require('./routes/user_routes');
+
+// Set up Express app
+const app = express();
+app.use(express.json());
+app.use(usersRouter);
+
+
+mongoose.set('strictQuery', false); // Add this line to suppress the deprecation warning
+
+// Set up database connection
+mongoose.connect('mongodb://localhost:27017/BackendAPI', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Failed to connect to MongoDB:', error);
+});
+
+
+// Define routes
+app.use('/user', usersRouter);
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
